4715813fa3491cfa3a950d23c0d6056296ee92bc
Ramache Zoubir <ramachezoubir@gmaail.com>
2024-06-01 20:27:41 +0100

    feat(chatModel) add chat models

---
+++ server/models/chatModels.tsx
@@ -0,0 +1,11 @@
+import { db } from "../megration/initial_megration"
+
+const createChat=(chatName:string ,userID:string )=>{
+    const query1 = `insert into SingleChat(chatName) values (${chatName})`
+    db.query(query1).run()
+    const chatID= (db.query( `select last_insert_rowid() as chatId`).get() as {chatID:number}).chatID
+    
+    const query2 = `insert into Chats(chatID,userID ) values (${chatID},${userID})`
+    db.query(query2).run()
+    
+}
