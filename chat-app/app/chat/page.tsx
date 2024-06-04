d8715e6086ea452c5be065cb384d9572ccc83c59
Ramache Zoubir <ramachezoubir@gmaail.com>
2024-06-04 15:33:41 +0100

    fix : auth bugs

--- chat-app/app/chat/page.tsx
+++ chat-app/app/chat/page.tsx
@@ -6,47 +6,63 @@ import Message from "@/components/shared/message";
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { MessageValidationSchema } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+
 interface MessageData {
   message: string;
   userID: string;
 }
 
-
 export default function Home() {
   const [messageList, setMassageList] = useState<MessageData[]>([]);
-  const { sendMessage , lastMessage } = useWebSocket(`ws://${process.env.NEXT_PUBLIC_SERVER_API}/ws` )
+  const { sendMessage, lastMessage } = useWebSocket(
+    `ws://${process.env.NEXT_PUBLIC_SERVER_API}/ws`
+  );
   const [MyMessage, setMyMessage] = useState<string>("");
-  
-  
-  const handleClick = () => {
-    sendMessage(MyMessage);
-    
-    setMyMessage('')
-    
-  };
+  const router = useRouter();
+
   const [cookie, setCookie] = useCookies(["userID"]);
+
+  const handleClick = () => {
+    if (MyMessage.trim().length) {
+      sendMessage(MyMessage);
+
+      setMyMessage("");
+    }
+  };
   useEffect(() => {
-    if (lastMessage){
-      const { message  , data:{userID}}=JSON.parse(lastMessage.data) 
+    if (!cookie.userID) {
+      router.push("/");
+    }
       
-      
-      setMassageList((prev) => [...prev, {message ,userID}] );
-    } 
+    if (lastMessage) {
+      const {
+        message,
+        data: { userID },
+      } = JSON.parse(lastMessage.data);
+
+      setMassageList((prev) => [...prev, { message, userID }]);
+    }
   }, [lastMessage]);
   return (
     <div className=" w-full   h-screen overflow-x-hidden overflow-y-scroll relative ">
-      {messageList.map(({message , userID}, index) =>   (
-        <Message recieved={cookie.userID !=userID} content={message} key={`${message}${index}`} />
+      {messageList.map(({ message, userID }, index) => (
+        <Message
+          recieved={cookie.userID != userID}
+          content={message}
+          key={`${message}${index}`}
+        />
       ))}
       {!lastMessage?.data && (
         <div className="  flex justify-center items-center h-full text-gray-500 text-xl font-bold">
-          there is no messages yet ! 
+          there is no messages yet !
         </div>
       )}
       <div className=" flex p-2  items-center   shadow-sm  fixed border-t-2 border-primary bg-accent  rounded-lg   w-full bottom-0  ">
         <Input
           placeholder="type something"
           value={MyMessage}
+          required
           onChange={(e) => setMyMessage(e.target.value)}
         />
         <button className=" m-6  " onClick={handleClick}>
